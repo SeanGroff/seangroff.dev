422e76088e6b6506ca72a7dfaf937e65550791ff
Sean Groff <sgroff04@gmail.com>
2023-02-25 09:52:39 -0600

    chore: init remix

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  rules: {
+    "sort-imports": [
+      "warn",
+      {
+        allowSeparatedGroups: true,
+      },
+    ],
+  },
+};
--- src/components/Introduction.story.md
+++
@@ -1,8 +0,0 @@
----
-icon: "carbon:bookmark"
-group: "top"
----
-
-# Welcome to My Shared Components
-
-This is a design system sandbox/display built with [Histoire](https://histoire.dev/).
